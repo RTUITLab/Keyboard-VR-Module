06d9f1c5accb783b3e179c5254f13f2792c3b666
Eloren1 <elorenn@bk.ru>
2021-05-26 21:42:02 +0300

    Add example scene

---
+++ Assets/Keyboard/Example Scene/KeyboardUsingExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardUsingExample : MonoBehaviour
+{
+    [SerializeField] private Keyboard keyboard; // Cсылка на клавиатуру.
+    [SerializeField] private InputField inputField; // Куда будем выводить текст.
+
+    private void Update()
+    {
+        string text = keyboard.Text; // Получаем значение введенного текста.
+
+        inputField.text = text; // В данном примере просто выводим его на экран.
+    }
+
+    private void Methods()
+    {
+        keyboard.ForceSetInput("Новый текст"); // Введенный текст заменится указанным.
+
+        keyboard.ClearAll(); // Введенный текст очистится до пустого поля.
+    }
+}
